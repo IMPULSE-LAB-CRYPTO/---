ca6fc74f0db690887dc92402ae4a6dec845f1875
илья <Oksimonov@mail.ru>
2024-10-24 18:05:01 +0300

    lab3_start

--- lab3/mai.py
+++ lab3/mai.py
@@ -1,1 +1,13 @@
-print("ddddd")
+import cv2
+import numpy as np
+
+img = cv2.imread('image.jpg')  # загрузка изображения
+
+cv2.imshow('girl', img)  # отображение
+cv2.waitKey(0)
+
+cv2.imwrite('image2.jpg', img)  # сохранение изображения
+
+img[0, 0, :] = 255  # замена значения пикселя
+shape = img.shape  # получение размера
+img2 = img.astype(np.uint8)  # преобразование типовв

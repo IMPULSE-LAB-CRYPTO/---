c620f32898ce50cba5b3f29a57a859b9e5f726d8
илья <Oksimonov@mail.ru>
2024-11-04 17:11:15 +0300

    Добавление функции чтения изображений (удалении блока теории)

--- lab3/mai.py
+++ lab3/mai.py
@@ -13,6 +13,14 @@ def parsing() -> argparse.Namespace:
     args = parser.parse_args()
     return args
 
+def read_image(input_image, greyscale):
+    """
+    Чтение изображения из файла
+    """
+    image = cv2.imread(input_image, greyscale)
+    if image is None:
+        raise FileExistsError
+    return image
 
 
 
@@ -20,37 +28,17 @@ def parsing() -> argparse.Namespace:
 
 
 
-img = cv2.imread('image.jpg')  # загрузка изображения
 
-cv2.imshow('girl', img)  # отображение
-cv2.waitKey(0)
-
-cv2.imwrite('image2.jpg', img)  # сохранение изображения
-
-img[0, 0, :] = 255  # замена значения пикселя
-shape = img.shape  # получение размера
-img2 = img.astype(np.uint8)  # преобразование типов
+def main():
+    args = parsing()
+    try:
+        image = read_image(args.input_image, cv2.IMREAD_GRAYSCALE)
+        print(image)
 
 
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
-plt.imshow(img)  # загрузка изображения
-plt.show()  # отображение
 
-# создание данных
-x = np.linspace(0, 10, 100)  # 100 точек от 0 до 10
-y = np.sin(x)  # нахождение синуса
-
-# создание графика
-plt.figure(figsize=(10, 5))  # размер графика
-
-plt.plot(x, y, label='sin(x)', color='blue')  # линейный график синуса
-plt.title('График синуса')  # заголовок графика
-plt.xlabel('x')  # подпись оси x
-plt.ylabel('sin(x)')  # подпись оси y
-plt.axhline(0, color='black',linewidth=0.5, ls='--')  # линия по оси x
-plt.axvline(0, color='black',linewidth=0.5, ls='--')  # линия по оси y
-plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)  # сетка
-plt.legend()  # легенда
-
-# отображение графика
-plt.show()
+if __name__ == "__main__":
+    main()

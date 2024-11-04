a0dd90c895606990114f29677d9b1d676bb4930a
илья <Oksimonov@mail.ru>
2024-11-04 15:51:20 +0300

    Теория для себя

--- lab3/mai.py
+++ lab3/mai.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt  # импорт
 
 img = cv2.imread('image.jpg')  # загрузка изображения
 
@@ -10,4 +11,28 @@ cv2.imwrite('image2.jpg', img)  # сохранение изображения
 
 img[0, 0, :] = 255  # замена значения пикселя
 shape = img.shape  # получение размера
-img2 = img.astype(np.uint8)  # преобразование типовв
+img2 = img.astype(np.uint8)  # преобразование типов
+
+
+
+plt.imshow(img)  # загрузка изображения
+plt.show()  # отображение
+
+# создание данных
+x = np.linspace(0, 10, 100)  # 100 точек от 0 до 10
+y = np.sin(x)  # нахождение синуса
+
+# создание графика
+plt.figure(figsize=(10, 5))  # размер графика
+
+plt.plot(x, y, label='sin(x)', color='blue')  # линейный график синуса
+plt.title('График синуса')  # заголовок графика
+plt.xlabel('x')  # подпись оси x
+plt.ylabel('sin(x)')  # подпись оси y
+plt.axhline(0, color='black',linewidth=0.5, ls='--')  # линия по оси x
+plt.axvline(0, color='black',linewidth=0.5, ls='--')  # линия по оси y
+plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)  # сетка
+plt.legend()  # легенда
+
+# отображение графика
+plt.show()

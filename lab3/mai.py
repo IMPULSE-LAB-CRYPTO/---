f8779f260c9137144d89a388429e0ba44986a6b7
илья <Oksimonov@mail.ru>
2024-11-04 21:14:04 +0300

    Доработка binary_image; Создание функции show_all; создание парсера и изменение логики функции для трех изображений

--- lab3/mai.py
+++ lab3/mai.py
@@ -8,8 +8,9 @@ def parsing() -> argparse.Namespace:
     Парсинг аргументов командной строки
     """
     parser = argparse.ArgumentParser(description="Работа с изображением")
-    parser.add_argument('path_to_download_image', type = str, help='path to the input image')
-    parser.add_argument('path_to_result_image', type = str, help='path to the output image')
+    parser.add_argument('path_to_original_image', type = str, help='path to the input image')
+    parser.add_argument('path_to_grey_image', type=str, help='path to the grey_output image')
+    parser.add_argument('path_to_binary_image', type = str, help='path to the binary_output image')
     args = parser.parse_args()
     return args
 
@@ -35,6 +36,7 @@ def gistogramm(image) -> None:
     plt.title("Grayscale Histogram")
     plt.xlabel("Bins")
     plt.ylabel("# of Pixels")
+    # Аргументы: 1-само изображение 2-канал(серый,зеленый,синий) 3-mask(срез), 4-количество интервалов, 5-диапозон на графике
     hist = cv2.calcHist([image], [0], None, [256], [0, 256])
     plt.plot(hist)
     plt.xlim([0, 256])
@@ -50,21 +52,52 @@ def task_binary_image(image):
     _, binary_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
     return binary_image
 
+def show_all(orig_image, grey_image, binary_image):
+
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 3, 1)
+    plt.title("Original Image")
+    plt.imshow(orig_image, cmap='gray')
+    plt.axis('off')
+
+    plt.subplot(1, 3, 2)
+    plt.title("Grey Image")
+    plt.imshow(grey_image, cmap='gray')
+    plt.axis('off')
+
+    plt.subplot(1, 3, 3)
+    plt.title("Binary Image")
+    plt.imshow(binary_image, cmap='gray')
+    plt.axis('off')
+
+    plt.show()
 
 def main():
     args = parsing()
     try:
-        image = write_image(args.path_to_download_image, cv2.IMREAD_GRAYSCALE)
-        cv2.imshow('Arcane', image)
+        orig_image = cv2.imread(args.path_to_original_image)
+
+        grey_image = write_image(args.path_to_original_image, cv2.IMREAD_GRAYSCALE)
+        cv2.imshow('Kitty', grey_image)
         cv2.waitKey(0)
 
+        cv2.imwrite(args.path_to_binary_image, grey_image)
+        print(f"Черно-белое изображение сохранено в lab3/new_image")
+
         # Вывод размера изображения
-        shape = image.shape
+        shape = grey_image.shape
         print(f"Размер изображения: {shape}")
 
         # Построение и отображение гистограммы
-        gistogramm(image)
+        gistogramm(grey_image)
 
+        binary_image = task_binary_image(grey_image)
+        cv2.imshow('black_and_white_kitty', binary_image)
+        cv2.waitKey(0)
+
+        # Показываю результат
+        show_all(orig_image, grey_image, binary_image)
 
 
 

5817ba1b2a1133e1648cdcd5a271e5ce1c426e20
илья <Oksimonov@mail.ru>
2024-11-04 21:41:12 +0300

    Доработка кода

--- lab3/mai.py
+++ lab3/mai.py
@@ -46,14 +46,24 @@ def task_binary_image(image):
     """
     Преобразование в бинарное изображение
     :param image:
-    :return:
+    :return: многомерный массив
     """
     # Преобразование изображения в бинарное
+    '''
+    Аргументы: 1-само изображение 2-пороговое значение(ниже пиксели черные, выше-белые)
+    3-значение присвоенное пикселям прошедшим порог 4-бинарное преобразование
+    '''
     _, binary_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
     return binary_image
 
-def show_all(orig_image, grey_image, binary_image):
-
+def show_all(orig_image, grey_image, binary_image) -> None:
+    """
+    Функция демонстрации всех трех изображений в одном окне
+    :param orig_image: Оригинальное изображение
+    :param grey_image: Изображение в диапозоне 256 бит(серое)
+    :param binary_image: Бинарное изображение
+    :return: None
+    """
     plt.figure(figsize=(10, 5))
 
     plt.subplot(1, 3, 1)
@@ -77,13 +87,15 @@ def main():
     args = parsing()
     try:
         orig_image = cv2.imread(args.path_to_original_image)
-
-        grey_image = write_image(args.path_to_original_image, cv2.IMREAD_GRAYSCALE)
-        cv2.imshow('Kitty', grey_image)
+        print(f"Оригинальное изображение сохранено в lab3/orig_image")
+        cv2.imshow('Standart_kitty', orig_image)
         cv2.waitKey(0)
 
-        cv2.imwrite(args.path_to_binary_image, grey_image)
-        print(f"Черно-белое изображение сохранено в lab3/new_image")
+        grey_image = write_image(args.path_to_original_image, cv2.IMREAD_GRAYSCALE)
+        cv2.imwrite(args.path_to_grey_image, grey_image)
+        print(f"Черно-белое изображение сохранено в lab3/grey_image")
+        cv2.imshow('Upset_kitty', grey_image)
+        cv2.waitKey(0)
 
         # Вывод размера изображения
         shape = grey_image.shape
@@ -93,14 +105,13 @@ def main():
         gistogramm(grey_image)
 
         binary_image = task_binary_image(grey_image)
+        cv2.imwrite(args.path_to_binary_image, binary_image)
+        print(f"Бинарное изображение сохранено в lab3/binary_image")
         cv2.imshow('black_and_white_kitty', binary_image)
         cv2.waitKey(0)
 
         # Показываю результат
         show_all(orig_image, grey_image, binary_image)
-
-
-
 
     except Exception as e:
         print(f"Произошла ошибка: {e}")

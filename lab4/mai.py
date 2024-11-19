d2a4f40a262fb5e8bd503f2fc00a59018602b1b1
илья <Oksimonov@mail.ru>
2024-11-19 22:24:31 +0300

    Добавление функций обработки изображений и добавления колонок, также проверка статистической ифнормации

--- lab4/mai.py
+++ lab4/mai.py
@@ -5,14 +5,52 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 
+
+
+def get_image_dimensions(path):
+    '''
+    Функция для получения размеров изображения
+    :param path:
+    :return: height, width, depth
+    '''
+    image = cv2.imread(path)
+    if image is not None:
+        height, width, depth = image.shape
+        return height, width, depth
+    else:
+        return None, None, None
+
+def add_columns(df):
+    '''
+    Добавление столбцов с размерами изображения
+    :return: None
+    '''
+    # Создание пустых списков для размеров изображений
+    heights = []
+    widths = []
+    depths = []
+
+    # Заполнение списков размерами изображений
+    for path in df['absolute_path']:
+        height, width, depth = get_image_dimensions(path)
+        heights.append(height)
+        widths.append(width)
+        depths.append(depth)
+
+    # Добавление столбцов с размерами изображений в DataFrame
+    df['height'] = heights
+    df['width'] = widths
+    df['depth'] = depths
+
+    return None
+
 def parsing() -> argparse.Namespace:
     """
     Парсинг аргументов командной строки
     """
     parser = argparse.ArgumentParser(description="Работа с изображением")
-    parser.add_argument('path_to_original_image', type = str, help='path to the input image')
-    parser.add_argument('path_to_grey_image', type=str, help='path to the grey_output image')
-    parser.add_argument('path_to_binary_image', type = str, help='path to the binary_output image')
+    parser.add_argument('path_to_images_folder', type = str, help='path to folder with images')
+    parser.add_argument('path_to_csv_file', type=str, help='path to the csv file')
     args = parser.parse_args()
     return args
 
@@ -20,8 +58,18 @@ def parsing() -> argparse.Namespace:
 def main():
     args = parsing()
     try:
-        df = pd.read_csv("data.csv")  # загрузка данных из csv файла
-        print(df.head())  # отображение первых строк таблицы
+        df = pd.read_csv(args.path_to_csv_file)  # загрузка данных из csv файла
+        print(df.head())
+        print()
+
+        add_columns(df)
+        print(df.head())
+
+        # Вычисляем статистическую информацию для столбцов размеров изображения
+        stats = df[['height', 'width', 'depth']].describe()
+        print(stats)
+
+
 
         print()
 
@@ -33,52 +81,3 @@ if __name__ == "__main__":
     main()
 
 
-
-
-
-# Предположим, что у вас есть список файлов изображений и их абсолютные пути
-file_paths = ["path/to/image1.jpg", "path/to/image2.jpg", "path/to/image3.jpg"]
-absolute_paths = [os.path.abspath(file_path) for file_path in file_paths]
-
-# Создаем DataFrame с двумя колонками: абсолютный и относительный пути
-df = pd.DataFrame({
-    'absolute_path': absolute_paths,
-    'relative_path': file_paths
-})
-
-# Добавляем колонки для высоты, ширины и глубины изображения
-heights = []
-widths = []
-depths = []
-
-for file_path in file_paths:
-    image = cv2.imread(file_path)
-    height, width, depth = image.shape
-    heights.append(height)
-    widths.append(width)
-    depths.append(depth)
-
-df['height'] = heights
-df['width'] = widths
-df['depth'] = depths
-
-# Вычисляем статистическую информацию для столбцов размеров изображения
-stats = df[['height', 'width', 'depth']].describe()
-print(stats)
-
-# Функция для фильтрации DataFrame по максимальной ширине и высоте изображения
-def filter_images(max_width, max_height):
-    return df[(df['height'] <= max_height) & (df['width'] <= max_width)]
-
-# Добавляем новый столбец с площадью изображения
-df['area'] = df['height'] * df['width']
-
-# Сортируем данные по площади изображений
-df = df.sort_values(by='area')
-
-# Создаем гистограмму распределения площадей изображений
-plt.hist(df['area'], bins=10, edgecolor='black')
-plt.title('Распределение площадей изображений')
-plt.xlabel('Площадь изображения')
-plt.ylabel('Количество изображений')
-plt.show()
